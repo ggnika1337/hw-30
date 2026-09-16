84034b893cd6f2322fb0bd8c05fceaa800c4c33e
ggnika1337 <nikushanika568@gmail.com>
2026-09-16 17:28:10 +0400

    internal server error

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -4,6 +4,7 @@ import { AuthController } from './auth.controller';
 import { JwtModule } from '@nestjs/jwt';
 import { UsersModule } from '../users/users.module';
 import { ConfigModule, ConfigService } from '@nestjs/config';
+import { EmailSenderModule } from 'src/email-sender/email-sender.module';
 
 @Module({
   imports: [
@@ -16,6 +17,7 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
         secret: configService.getOrThrow<string>('JWT_SECRET'),
       }),
     }),
+    EmailSenderModule,
   ],
   controllers: [AuthController],
   providers: [AuthService],
--- src/email-sender/email-sender.module.ts
+++ src/email-sender/email-sender.module.ts
@@ -1,7 +1,21 @@
 import { Module } from '@nestjs/common';
+import { MailerModule } from '@nestjs-modules/mailer';
 import { EmailSenderService } from './email-sender.service';
 
 @Module({
+  imports: [
+    MailerModule.forRoot({
+      transport: {
+        host: 'smtp.gmail.com',
+        port: 587,
+        secure: false,
+        auth: {
+          user: process.env.EMAIL_USER,
+          pass: process.env.EMAIL_PASS,
+        },
+      },
+    }),
+  ],
   providers: [EmailSenderService],
   exports: [EmailSenderService],
 })
